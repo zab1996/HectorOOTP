a47293bfbb16ddf922c501abcb1b0d4e93310413
zab1996 <20804331+zab1996@users.noreply.github.com>
2025-07-27 19:12:41 -0400

    Delete Hector 2.1.1 Source Code directory

--- Hector 2.1.1 Source Code/main.py
+++
@@ -1,9 +0,0 @@
-from gui.core import build_gui
-
-if __name__ == "__main__":
-    try:
-        build_gui()
-    except Exception as e:
-        import traceback
-        traceback.print_exc()
-        input("Press Enter to exit...")
